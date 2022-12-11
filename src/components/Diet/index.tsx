36c07c8fe134c503e5644cda4b18d3cf5e4fa283
Joao <joaoviniciusvitral@hotmail.com>
2022-12-11 04:05:49 -0300

    updates
    Co-authored-by: Marcelo Bracet <marcelo.bracet1@gmail.com>

--- src/components/Diet/index.tsx
+++ src/components/Diet/index.tsx
@@ -1,53 +1,136 @@
 import React, { useState } from "react";
-import { Flex, Text, Button, Select, Input } from "@chakra-ui/react";
+import {
+  Flex,
+  Text,
+  Button,
+  Select,
+  Input,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { QuestionIcon } from "@chakra-ui/icons";
 
 export const Diet: React.FC = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const [height, setHeight] = useState<string>('')
-  const [weight, setWeight] = useState<string>('')
-  const [goal, setGoal] = useState<string>('')
+  const [height, setHeight] = useState<number>(0);
+  const [weight, setWeight] = useState<number>(0);
+  const [goal, setGoal] = useState<string>("");
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setGoal(e.target.value)
-  }
+    setGoal(e.target.value);
+  };
+
+  const openModal = () => {
+    alert("Em breve");
+  };
 
   const calculateImc = () => {
-    const imc = weight / (height * height)
-    return imc.toFixed(2)
-  }
+    const imc = weight / (height * height);
+    return imc.toFixed(2);
+  };
 
   return (
-    <Flex flexDir='column' w="100%" h="100%">
-        <Text as='h1' fontWeight='400' m='15px auto'>Qual o seu objetivo?</Text>
-      <Flex flexDir='column' gap='20px' w='80%' align='center' justify='center' m='10px auto'>
-      <Select
-        value={goal}
-        onChange={handleChange}
-        borderColor='#000'
-        borderSize='3px'
-        w='250px'
-        variant='flushed'
-        color='#000'
-        placeholder='Selecione seu objetivo!'
->
-  <option value='emagrecer'>Emagrecer</option>
-  <option value='resistencia'>Resistência</option>
-  <option value='hipertrofia'>Hipertrofia</option>
-</Select>
-{goal &&
-<Flex gap='5px'>
-<Input w='250px' onChange={e =>setHeight(e.target.value)} value={height} placeholder='Altura. Ex. (1.71)'/>
-<Input w='250px' onChange={e => setWeight(e.target.value)} value={weight} placeholder='Peso atual. Ex. (75.6)'/>
-</Flex>
-}
-{goal && height && weight &&
-<Flex flexDir='column' gap='5px' align='center' justify='center'>
-<Text as='h1' fontWeight='400' m='15px auto'>Seu IMC é: {calculateImc()}</Text>
-<Text as='h1' fontWeight='400' m='15px auto'>Seu objetivo é: {goal}</Text>
-</Flex>
-}
-</Flex>
-<Flex position='absolute' bgColor='red' w='20px' bottom='0' right='0'></Flex>
-</Flex>
+    <Flex flexDir="column" w="100%">
+      <Text as="h1" fontWeight="400" m="15px auto">
+        Qual o seu objetivo?
+      </Text>
+      <Flex
+        flexDir="column"
+        gap="20px"
+        w="80%"
+        align="center"
+        justify="center"
+        m="10px auto"
+      >
+        <Select
+          value={goal}
+          onChange={handleChange}
+          borderColor="#000"
+          w="250px"
+          variant="flushed"
+          color="#000"
+          placeholder="Selecione seu objetivo!"
+        >
+          <option value="emagrecer">Emagrecer</option>
+          <option value="resistencia">Resistência</option>
+          <option value="hipertrofia">Hipertrofia</option>
+        </Select>
+        {goal && (
+          <Flex gap="5px">
+            <Input
+              w="250px"
+              onChange={(e) => setHeight(e.target.value)}
+              value={height}
+              placeholder="Altura. Ex. (1.71)"
+            />
+            <Input
+              w="250px"
+              onChange={(e) => setWeight(e.target.value)}
+              value={weight}
+              placeholder="Peso atual. Ex. (75.6)"
+            />
+          </Flex>
+        )}
+        {goal && height && weight && (
+          <Flex flexDir="column" gap="5px" align="center" justify="center">
+            <Text as="h1" fontWeight="400" m="15px auto">
+              Seu IMC é: {calculateImc()}
+            </Text>
+            <Text as="h1" fontWeight="400" m="15px auto">
+              Seu objetivo é: {goal}
+            </Text>
+          </Flex>
+        )}
+
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>IMC - O que significa?</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Text as='p'>
+                O IMC é um cálculo simples que permite avaliar se a pessoa está
+                dentro do peso que é considerado ideal para a sua altura. Também
+                conhecido como Índice de Massa Corporal, o IMC é uma fórmula
+                utilizada por vários profissionais de saúde, incluindo médicos,
+                enfermeiros e nutricionistas, para saber, de uma forma rápida,
+                se a pessoa precisa ganhar ou perder peso. Embora seja uma
+                ferramenta muito comum, o IMC não é considerado a forma mais
+                exata de avaliar o peso, já que não leva em consideração a
+                composição corporal. Por isso, é comum que no caso de atletas
+                (que possuem uma maior quantidade de massa muscular) seja
+                aconselhado o uso de outras técnicas, como a bioimpedância, para
+                uma avaliação mais detalhada do peso
+              </Text>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Close
+              </Button>
+              <Button variant="ghost">Secondary Action</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+        <Button
+          w="50px"
+          position="absolute"
+          bottom="100px"
+          right="10px"
+          colorScheme="blue"
+          variant="outline"
+          onClick={onOpen}
+        >
+          <QuestionIcon />
+        </Button>
+      </Flex>
+    </Flex>
   );
 };
--- src/components/WorkoutCollection/index.tsx
+++ src/components/WorkoutCollection/index.tsx
@@ -8,16 +8,19 @@ const SeriesMocked = [
     id: 0,
     title: "Treino A",
     workout: ["Peito", "Costas"],
+    url: "https://images.unsplash.com/photo-1541534741688-6078c6bfb5c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
   },
   {
     id: 1,
     title: "Treino B",
     workout: ["Perna", "Triceps"],
+    url: "https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
   },
   {
     id: 2,
     title: "Treino C",
     workout: ["Biceps", ""],
+    url: "https://images.unsplash.com/photo-1526408984842-5f1323d42469?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
   },
 ];
 
@@ -40,7 +43,15 @@ export const WorkoutCollection: React.FC = () => {
             alignItems: "center",
           }}
         >
-          <Flex h="65px" w="350px" border="1px solid #000">
+          <Flex
+            background={`url(${item.url})`}
+            backgroundSize='cover'
+            backgroundRepeat='no-repeat'
+            backgroundPosition='center'
+            h="100px"
+            w="350px"
+            border="1px solid #000"
+          >
             {item.title}
           </Flex>
         </SwiperSlide>
